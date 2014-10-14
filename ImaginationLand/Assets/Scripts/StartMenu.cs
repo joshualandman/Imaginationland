dabef7acd77bb9cddae4597579d106a77db045dc
jsl9284@rit.edu <jsl9284@rit.edu>
2014-10-14 15:58:59 -0400

    Character Movement Oneway platforms Malleable Platforms Gui Cape Hook set up

---
+++ ImaginationLand/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+	public bool isClicked = false;
+
+	void Update()
+	{
+		if(isClicked)
+		{
+			Application.LoadLevel("Instructions");
+			isClicked = false;
+		}
+	}
+
+	void OnGUI()
+	{
+		if(GUI.Button( new Rect(Screen.height/2, Screen.width/2, 300, 100), "START"))
+		{
+			isClicked = true;
+		}
+	}
+
+}
