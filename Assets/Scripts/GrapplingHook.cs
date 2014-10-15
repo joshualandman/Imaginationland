cf27b9d553390996c9d7e48624ef56aa37d7598d
JoshuaLandman <jsl9284@rit.edu>
2014-10-15 08:24:34 -0400

    commit test

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -5,6 +5,16 @@ public class GrapplingHook : MonoBehaviour {
 
 	public GameObject hook;
 	private bool isHookFired = false;
+	public GameObject player;
+	public LineRenderer rope;
+
+	private Vector3 hookStart = new Vector3(0,-20,0);
+	private double timer = 0.0;
+
+	void Start()
+	{
+		rope.enabled = false;
+	}
 
 	void Update()
 	{
@@ -12,11 +22,33 @@ public class GrapplingHook : MonoBehaviour {
 
 		Vector3 pos = hook.transform.position;
 
-
 		if(isHookFired)
 		{
-			pos.x+= 0.1f;
+			timer++;
+			rope.enabled = true;
+			pos.x += 0.3f;
 			hook.transform.position = pos;
+
+			if(timer >= 30.0)
+			{
+				//time limit
+				//rope.enabled=false;
+				//hook.transform.position = hookStart;
+
+				//keep hook out
+				hook.transform.position = hook.transform.position;
+				timer = 0;
+				isHookFired = false;
+			}
+		}
+		rope.SetPosition(0, player.transform.position);
+		rope.SetPosition(1, hook.transform.position);
+
+		if(Input.GetKeyUp(KeyCode.T))
+		{
+			hook.transform.position = hookStart;
+			rope.enabled = false;
+			isHookFired = false;
 		}
 	}
 
@@ -24,8 +56,18 @@ public class GrapplingHook : MonoBehaviour {
 	{
 		if(Input.GetKeyUp(KeyCode.G))
 		{
-			Instantiate(hook, new Vector3(transform.position.x, transform.position.y, transform.position.z), new Quaternion(0,0,0,0));
+			hook.transform.position = transform.position;
+			//Instantiate(rope, hook.transform.position, new Quaternion(0,0,0,0));
+			//Instantiate(hook, new Vector3(transform.position.x, transform.position.y, transform.position.z), new Quaternion(0,0,0,0));
 			isHookFired = true;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.tag == "Grounded")
+		{
+			//hook.rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
 		}
 	}
 }
